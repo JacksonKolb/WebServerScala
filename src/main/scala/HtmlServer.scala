8cb63c8b11efff86c08e712dad0145354464a772
Drew Engberson <drew.engberson@gmail.com>
2019-03-02 18:38:00 -0700

    Fix build errors.

--- src/main/scala/HtmlServer.scala
+++ src/main/scala/HtmlServer.scala
@@ -8,57 +8,61 @@ import java.io._
 import java.util.Scanner
 import java.lang.StringBuilder
 
+
 object WebServer {
+
   def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("my-system")
+    implicit val system = ActorSystem("datboi")
     implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    val route = 
-    	path("hello") {
-	    	get {
-	      		complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Hi</h1>"))
-	    	}
-	  	}
+    while (true) {
 
-	val bindingFuture = Http().bindAndHandle(route, "localhost", 8000)
-
-    while(true){
-
-		println(s"Server online at http://localhost:8000/" +
-				"\nEnter a file name, or press Q to quit...")
-
+    	println("Hey betch, enter a file name or press Q to quit: ")		
+		
 		val input = StdIn.readLine()
 
-		if(input == "Q" || input == "q") {
-			bindingFuture
-	      	.flatMap(_.unbind()) 
-	      	.onComplete(_ => system.terminate()) 
+		if (input.equals("Q") || input.equals("q")) {
+			System.exit(0)
 		}
-		else {
-			val html = string_to_file(input)
-			val route = path("hello") {
-	    		get {
-	      			complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, html))
-	    		}
-	  		}
-	  		val bindingFuture = Http().bindAndHandle(route, "localhost", 8000)
-		}
+
+		val html = string_to_file(input)
+		println(html)			
+			
+		val route = path("hello") {
+    		get {
+      			complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, html))
+    		}
+  		}
+  		val bindingFuture = Http().bindAndHandle(route, "localhost", 8000)
+	
+  		println(s"Server online at http://localhost:8000/")
+
+  		StdIn.readLine()
     }    
   }
 
+
   def string_to_file(file_name: String): String = {
-  	val path = "C:\\Users\\drewe\\Desktop\\CS390\\homework2\\src\\main\\scala\\" + file_name
-  	val file = new File(path)
+
+  	val path = "C:\\Users\\drewe\\Desktop\\ScalaServer\\src\\main\\scala\\" + file_name
+  	var file = new File("")
+
+  	try {
+  		file = new File(path)
+  	}
+  	catch  {
+  		case e: Exception => println("File not found: " + path)
+  	}
+  	
   	val scanner = new Scanner(file)
-  	val html = ""
+  	val builder = new StringBuilder()
 
   	while(scanner.hasNext()) {
-  		html = html + scanner.Next().ToString()
+  		builder.append(scanner.next())
   	}
 
-  	return html
+  	return builder.toString()
   }
 }
